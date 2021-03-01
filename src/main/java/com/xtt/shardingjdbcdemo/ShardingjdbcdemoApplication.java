d7bed97779ce2b81a20af0e0c3bface978ddd42a
huMonster <monster.hu@outlook.com>
2021-03-01 17:52:41 +0800

    sharding-jdbc

---
+++ src/main/java/com/xtt/shardingjdbcdemo/ShardingjdbcdemoApplication.java
@@ -0,0 +1,13 @@
+package com.xtt.shardingjdbcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingjdbcdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingjdbcdemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/xtt/shardingjdbcdemo/entity/Course.java
@@ -0,0 +1,11 @@
+package com.xtt.shardingjdbcdemo.entity;
+
+/**
+ * @Description Course
+ * @Author Monster
+ * @Date 2021/3/1 17:29
+ * @Version 1.0
+ */
+public class Course {
+
+}
